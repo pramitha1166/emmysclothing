72982a5c2e35844c1a983479dd2c37d8be2d5727
pramitha1166 <pramitha.wanigarathne@gmail.com>
2022-02-18 01:01:30 +0530

    do some changes

---
+++ lib/presentaion/animation/fade_animation.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class FadeAnimation extends StatelessWidget {
+  final double delay;
+  final double child;
+
+  const FadeAnimation({Key? key, required this.child, required this.delay})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+/*
+    final tween = MultiTween([
+      
+    ])
+
+*/
+    return Container();
+  }
+}
