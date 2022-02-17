63e77cacbe806b9c445af124886f125887b368f7
pramitha1166 <pramitha.wanigarathne@gmail.com>
2022-02-17 23:44:59 +0530

    do some changes

--- lib/screen/cart_screen.dart
+++ lib/screen/cart_screen.dart
@@ -11,7 +11,7 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text("Cart Screen new "),
+      child: Text("Cart Screen new 1"),
     );
   }
 }
